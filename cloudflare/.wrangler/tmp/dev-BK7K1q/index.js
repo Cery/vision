c7a004ba5f8d42177fa2a79dd6271917cee764b9
Cery <946320220@qq.com>
2025-12-01 13:38:01 +0900

    更新需求市场

--- cloudflare/.wrangler/tmp/dev-BK7K1q/index.js
+++ cloudflare/.wrangler/tmp/dev-BK7K1q/index.js
@@ -1665,7 +1665,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-Nn0bot/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-GP2o3S/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -1697,7 +1697,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-Nn0bot/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-GP2o3S/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
